c7551e16b1b84d013363083726bad6a959ac93c7
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-06 15:41:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 
 wb = load_workbook('Prueba.ods')
 ws = wb.active
 
 wb.save('Prueba.ods')
+
+
+class sheet:
+    def __init__(self, name):
+        self.name = name
+        self.column = []
+
+    def add_column(self, name):
+        self.column.append(name)
